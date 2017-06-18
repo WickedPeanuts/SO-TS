670d24c8c07bec79542a40fbdaa36287eb3795f7
Carlos H. M. S. <carlos_judo@hotmail.com>
2017-06-18 12:30:45 -0300

    Main Para resultados do pso

--- src/br/poli/sots/main.java
+++ src/br/poli/sots/main.java
@@ -19,6 +19,7 @@ import br.poli.sots.swarmintelligence.utils.MeanSquareError;
 import br.poli.sots.swarmintelligence.utils.RootMeanSquareError;
 import br.poli.sots.utils.logging.StaticLogger;
 import br.poli.sots.utils.serie.EOptimizer;
+import br.poli.sots.utils.serie.Serie;
 import br.poli.sots.utils.serie.Series;
 
 public class main {
@@ -26,7 +27,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		RunAllPSOSimulations();
+		RunAllPSOSimulations(Series.sobradinho);
 		StaticLogger.saveToFile();
 	}
 	
@@ -60,41 +61,43 @@ public class main {
 		System.out.println(particlePos[0] + " " + particlePos[1] + " " + particlePos[2] + " " + particlePos[3]);
 	}
 	
-	public static void RunAllPSOSimulations(){
+	public static void RunAllPSOSimulations(Serie serie){
 		int i = 1;
-		PSO(ETopology.Global, EFunction.MeanSquareError, EConstrictionFactor.FixedInertia);
+		PSO(ETopology.Global, EFunction.MeanSquareError, EConstrictionFactor.FixedInertia, serie);
 		System.out.println(i++ + "/9 PSO simulations complete!");
-		PSO(ETopology.Focal, EFunction.MeanSquareError, EConstrictionFactor.FixedInertia);
+		PSO(ETopology.Focal, EFunction.MeanSquareError, EConstrictionFactor.FixedInertia, serie);
 		System.out.println(i++ + "/9 PSO simulations complete!");
-		PSO(ETopology.Ring, EFunction.MeanSquareError, EConstrictionFactor.FixedInertia);
+		PSO(ETopology.Ring, EFunction.MeanSquareError, EConstrictionFactor.FixedInertia, serie);
 		System.out.println(i++ + "/9 PSO simulations complete!");
-		PSO(ETopology.Global, EFunction.MeanSquareError, EConstrictionFactor.FloatingInertia);
+		PSO(ETopology.Global, EFunction.MeanSquareError, EConstrictionFactor.FloatingInertia, serie);
 		System.out.println(i++ + "/9 PSO simulations complete!");
-		PSO(ETopology.Focal, EFunction.MeanSquareError, EConstrictionFactor.FloatingInertia);
+		PSO(ETopology.Focal, EFunction.MeanSquareError, EConstrictionFactor.FloatingInertia, serie);
 		System.out.println(i++ + "/9 PSO simulations complete!");
-		PSO(ETopology.Ring, EFunction.MeanSquareError, EConstrictionFactor.FloatingInertia);
+		PSO(ETopology.Ring, EFunction.MeanSquareError, EConstrictionFactor.FloatingInertia, serie);
 		System.out.println(i++ + "/9 PSO simulations complete!");
-		PSO(ETopology.Global, EFunction.MeanSquareError, EConstrictionFactor.ClercConstriction);
+		PSO(ETopology.Global, EFunction.MeanSquareError, EConstrictionFactor.ClercConstriction, serie);
 		System.out.println(i++ + "/9 PSO simulations complete!");
-		PSO(ETopology.Focal, EFunction.MeanSquareError, EConstrictionFactor.ClercConstriction);
+		PSO(ETopology.Focal, EFunction.MeanSquareError, EConstrictionFactor.ClercConstriction, serie);
 		System.out.println(i++ + "/9 PSO simulations complete!");
-		PSO(ETopology.Ring, EFunction.MeanSquareError, EConstrictionFactor.ClercConstriction);
+		PSO(ETopology.Ring, EFunction.MeanSquareError, EConstrictionFactor.ClercConstriction, serie);
 		System.out.println(i++ + "/9 PSO simulations complete!");
 		
 	}
 	
-	public static void PSO(ETopology topology, EFunction function, EConstrictionFactor constriction){
+	public static boolean firstTry = false;
+	
+	public static void PSO(ETopology topology, EFunction function, EConstrictionFactor constriction, Serie serie){
 		StaticLogger.add(topology + ", " + function + ", " + constriction + "\n", EOptimizer.PSO);
 		
 		for (int bkwrd = 0; bkwrd <= 3; bkwrd++){
 			for (int ffrd = 1; ffrd <= 3; ffrd++){
-				
+				if (firstTry && bkwrd == 0) continue;
 				//Adicionar label do logger
 				StaticLogger.add("Feedfoward: " + ffrd + ", Backward: " + bkwrd, EOptimizer.PSO);
 				System.out.println("Feedfoward: " + ffrd + ", Backward: " + bkwrd);
 				
 				//Adição da média da lista de convergências
-				Series.armaSerie = new Arma(Series.emborcacao, 120, ffrd, bkwrd);
+				Series.armaSerie = new Arma(serie, 120, ffrd, bkwrd);
 				
 				Swarm s = new Swarm(topology, function, constriction);
 				
@@ -113,7 +116,7 @@ public class main {
 			    StaticLogger.add(bestErrorList, EOptimizer.PSO, AbstractPSOParticle.functionType + " per iteration: {", "}");
 			    
 			    //MSE da best			
-			    Series.armaSerie = new Arma(Series.emborcacao, 120, ffrd, bkwrd);
+			    Series.armaSerie = new Arma(serie, 120, ffrd, bkwrd);
 				Series.armaSerie.setParameters(AbstractPSOParticle.positionGBest);
 				Series.armaSerie.forecastAll();
 				StaticLogger.add(AbstractPSOParticle.positionGBest, EOptimizer.PSO, "Parameters (" + ffrd + "Feedfoward, " + ffrd + " Backward) : {", "}");
@@ -124,15 +127,15 @@ public class main {
 				
 				//Erro quadrático médio (best) desazonalizado
 				Double mseError = MeanSquareError.instance.calculateFitness(AbstractPSOParticle.positionGBest);
-				StaticLogger.add(mseError, EOptimizer.PSO, "Best Mean (Deseasonalized) square error: ", "");
+				StaticLogger.add(mseError, EOptimizer.PSO, "Best Mean (Deseasonalized) mse: ", "");
 				
 				//Erro absoluto médio (best) desazonalizado
 				Double maeError = MeanAbsoluteError.instance.calculateFitness(AbstractPSOParticle.positionGBest);
-				StaticLogger.add(maeError, EOptimizer.PSO, "Best Mean (Deseasonalized) absolute error: ", "");
+				StaticLogger.add(maeError, EOptimizer.PSO, "Best Mean (Deseasonalized) mae: ", "");
 				
 				//Erro raiz quadrático (best) desazonalizado
 				Double rmseError = RootMeanSquareError.instance.calculateFitness(AbstractPSOParticle.positionGBest);
-				StaticLogger.add(rmseError, EOptimizer.PSO, "Best Mean (Deseasonalized) absolute error: ", "");
+				StaticLogger.add(rmseError, EOptimizer.PSO, "Best Mean (Deseasonalized) rmse: ", "");
 				
 				//As séries desazonalizadas
 				Series.armaSerie.seasonalizeSerie();
@@ -141,19 +144,21 @@ public class main {
 				
 				//Erro quadrático médio (best) sazonalizado
 				mseError = MeanSquareError.instance.calculateFitness(AbstractPSOParticle.positionGBest);
-				StaticLogger.add(mseError, EOptimizer.PSO, "Best Mean (Seasonalized) square error: ", "");
+				StaticLogger.add(mseError, EOptimizer.PSO, "Best Mean (Seasonalized) mse: ", "");
 				
 				//Erro absoluto médio (best) sazonalizado
 				maeError = MeanAbsoluteError.instance.calculateFitness(AbstractPSOParticle.positionGBest);
-				StaticLogger.add(maeError, EOptimizer.PSO, "Best Mean (Seasonalized) absolute error: ", "");
+				StaticLogger.add(maeError, EOptimizer.PSO, "Best Mean (Seasonalized) mae: ", "");
 				
 				//Erro raiz quadrático (best) desazonalizado
 				rmseError = RootMeanSquareError.instance.calculateFitness(AbstractPSOParticle.positionGBest);
-				StaticLogger.add(rmseError, EOptimizer.PSO, "Best Mean (Deseasonalized) absolute error: ", "");
+				StaticLogger.add(rmseError, EOptimizer.PSO, "Best Mean (Seasonalized) rmse: ", "");
 				
 				StaticLogger.add("\n", EOptimizer.PSO);
 			}
 		}
+		
+		firstTry = true;
 		
 		StaticLogger.add("\n\n\n", EOptimizer.PSO);
 	}
